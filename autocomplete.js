05172f84a8d63ddf3f46c91912f375af41304b89
Boaz Leskes <b.leskes@gmail.com>
2012-12-14 22:02:39 +0100

    Added auto complete to path box. Some layouting tweak

---
+++ autocomplete.js
@@ -0,0 +1,10 @@
+
+
+var GLOBAL_END_POINTS = [
+    '_cluster/nodes/stats',
+    '_percolator',
+    '_search',
+    '_stats',
+    '_status'
+];
+
--- sense.js
+++ sense.js
@@ -1,6 +1,54 @@
 
+var editor,output;
+
+
+function init () {
+  editor = ace.edit("editor");
+  editor.getSession().setMode("ace/mode/json");
+  editor.getSession().setFoldStyle('markbeginend');
+  output = ace.edit("output");
+  output.getSession().setMode("ace/mode/json");
+  output.getSession().setFoldStyle('markbeginend');
+  output.setTheme("ace/theme/monokai");
+
+  $("#es_host").val(window.location.hostname);
+  $("#es_path").typeahead({ "source" : GLOBAL_END_POINTS });
+
+}
+
+$(document).ready(init);
+
+function getCurrentTokenPath() {
+  var pos = editor.getCursorPosition();
+  var tokenIter = new (ace.require("ace/token_iterator").TokenIterator)(editor.getSession(),pos.row,pos.column);
+  var ret = "", last_var = "";
+  for (var t = tokenIter.getCurrentToken();t; t = tokenIter.stepBackward()) {
+    switch (t.type) {
+      case "paren.lparen":
+          if (t.value == "[" && !ret) {
+            // bottom of the chain, list element not intresting
+            ret = "[";
+          }
+          else
+            ret = t.value + last_var + ret;
+
+        last_var = "";
+        break;
+      case "string":
+      case "constant.numeric" :
+      case "variable":
+        if (!last_var) {
+          last_var = t.value.trim().replace(/"/g,'');
+        }
+        break;
+    }
+  }
+  return ret;
+}
 
 function callES() {
+  output.getSession().setValue('{ "__mode__" : "Calling ES...." }');
+
  var es_host = $("#es_host").val(),
      es_path = $("#es_path").val(),
      es_method = $("#es_method").val();
@@ -24,10 +72,10 @@ function callES() {
          catch (e) {
 
          }
-         output.getSession(). setValue(value);
+         output.getSession().setValue(value);
        }
        else {
-         output.setValue("Request failed to get to the server: " + status);
+         output.getSession().setValue("Request failed to get to the server: " + status);
        }
 
      }
