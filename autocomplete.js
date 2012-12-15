fb86ad8e791d676bb54b70a06923de476dba4c27
Boaz Leskes <b.leskes@gmail.com>
2012-12-15 21:41:53 +0100

    very first auto complete implementation

--- autocomplete.js
+++ autocomplete.js
@@ -1,10 +1,22 @@
 
 
-var GLOBAL_END_POINTS = [
-    '_cluster/nodes/stats',
-    '_percolator',
-    '_search',
-    '_stats',
-    '_status'
-];
+var ES_SCHEME_BY_ENDPOINT = {
+    '_cluster/nodes/stats' : {
+
+    },
+    '_percolator' : {
+
+    },
+    '_search' : {
+      autocomplete_rules: {
+          "{" : [ "query","facets","size","from","search_type","fields","partial_fields","highlight" ],
+          "{query{" : [ "term","match_all" ]
+      }
+    },
+    '_stats' : {
+    },
+    '_status' : {
+
+    }
+};
 
--- sense.js
+++ sense.js
@@ -1,38 +1,85 @@
 
-var editor,output;
+var editor,output,active_scheme = {};
+
+function autocomplete(editor) {
+  var tokenPath = getCurrentTokenPath(editor);
+  var autoCompleteRules = active_scheme.autocomplete_rules || {};
+  console.log("Starting auto complete for path: " + tokenPath + " options: " + autoCompleteRules[tokenPath]);
+  if (!autoCompleteRules[tokenPath]) return; // nothing to do..
 
 
-function init () {
-  editor = ace.edit("editor");
-  editor.getSession().setMode("ace/mode/json");
-  editor.getSession().setFoldStyle('markbeginend');
-  output = ace.edit("output");
-  output.getSession().setMode("ace/mode/json");
-  output.getSession().setFoldStyle('markbeginend');
-  output.setTheme("ace/theme/monokai");
+  var pos = editor.getCursorPosition();
+  var session = editor.getSession();
+  var currentToken = session.getTokenAt(pos.row,pos.column);
+  var tokenRange = new (ace.require("ace/range").Range)(pos.row,currentToken.start,pos.row,
+                                                        currentToken.start + currentToken.value.length);
+  var cursor = $(".ace_cursor");
+  var offset = cursor.offset();
+  var ac_input = $('<input id="autocomplete" type="text" data-provide="typeahead" />').appendTo($("#main"));
+  ac_input.css("left",offset.left);
+  ac_input.css("top",offset.top);
+  if (currentToken.type == "string" || currentToken.type == "variable") // string with current token
+    ac_input.val(currentToken.value.trim().replace(/"/g,''));
+  else
+    ac_input.val();
 
-  $("#es_host").val(window.location.host);
-  $("#es_path").typeahead({ "source" : GLOBAL_END_POINTS });
+  ac_input.css('visibility', 'visible');
+  ac_input.typeahead({
+    minLength : 0,
+    source : autoCompleteRules[tokenPath],
+    updater : function (item) {
+      switch (currentToken.type) {
+        case "paren.rparen":
+          editor.insert(', "'+item+'"');
+          break;
+        case "string":
+        case "variable":
+          session.replace(tokenRange,'"'+item+'"');
+          break;
+        default:
+          editor.insert('"'+item+'"');
+      }
 
+      ac_input.remove();
+      editor.focus();
+    }
+  });
+  ac_input.blur(function () {ac_input.css('visibility','hidden'); ac_input.remove()});
+
+  ac_input.focus();
 }
 
-$(document).ready(init);
 
-function getCurrentTokenPath() {
+function getCurrentTokenPath(editor) {
   var pos = editor.getCursorPosition();
   var tokenIter = new (ace.require("ace/token_iterator").TokenIterator)(editor.getSession(),pos.row,pos.column);
   var ret = "", last_var = "";
   for (var t = tokenIter.getCurrentToken();t; t = tokenIter.stepBackward()) {
     switch (t.type) {
       case "paren.lparen":
-          if (t.value == "[" && !ret) {
-            // bottom of the chain, list element not intresting
-            ret = "[";
+          if (!ret) {
+            // bottom of the chain, last var is not part of path.
+            ret = t.value;
           }
           else
             ret = t.value + last_var + ret;
 
         last_var = "";
+        break;
+      case "paren.rparen":
+          var parenCount =1;
+          for (t = tokenIter.stepBackward(); t && parenCount > 0 ; t = tokenIter.stepBackward()) {
+            switch (t.type) {
+              case "paren.lparen":
+                parenCount--;
+                break;
+              case "paren.rparen":
+                parenCount++;
+                break;
+            }
+          }
+          if (!t) // oops we run out.. we don't what's up return null;
+            return null;
         break;
       case "string":
       case "constant.numeric" :
@@ -44,6 +91,18 @@ function getCurrentTokenPath() {
     }
   }
   return ret;
+}
+
+function updateActiveScheme(endpoint) {
+  for (var scheme_endpoint in ES_SCHEME_BY_ENDPOINT) {
+      if (endpoint.indexOf(scheme_endpoint) == 0) {
+        endpoint = scheme_endpoint;
+        break;
+      }
+    }
+
+  active_scheme = ES_SCHEME_BY_ENDPOINT[endpoint];
+
 }
 
 function callES() {
@@ -81,3 +140,59 @@ function callES() {
      }
   })
 }
+
+function reformat() {
+  var value = editor.getSession().getValue();
+  try {
+     value = JSON.stringify(JSON.parse(value), null, 3);
+     editor.getSession().setValue(value);
+  }
+  catch (e) {
+
+  }
+
+}
+
+function init () {
+  editor = ace.edit("editor");
+  editor.getSession().setMode("ace/mode/json");
+  editor.getSession().setFoldStyle('markbeginend');
+  editor.commands.addCommand({
+      name: 'autocomplete',
+      bindKey: {win: 'Ctrl-Space',  mac: 'Ctrl-Space'},
+      exec: autocomplete
+  });
+  editor.commands.addCommand({
+      name: 'reformat editor',
+      bindKey: {win: 'Ctrl-I',  mac: 'Command-I'},
+      exec: reformat
+  });
+  output = ace.edit("output");
+  output.getSession().setMode("ace/mode/json");
+  output.getSession().setFoldStyle('markbeginend');
+  output.setTheme("ace/theme/monokai");
+  output.setReadOnly(true);
+
+  reformat();
+  editor.focus();
+
+
+  $("#es_host").val(window.location.host);
+  var paths = [];
+  for (var endpoint in ES_SCHEME_BY_ENDPOINT) {
+    paths.push(endpoint);
+  }
+  paths.sort();
+
+
+  var es_path = $("#es_path");
+  es_path.typeahead({ "source" : paths });
+
+  es_path.change(function () {
+    updateActiveScheme(es_path.val());
+  });
+
+  es_path.change(); // initialized using baked in value.
+}
+
+$(document).ready(init);
