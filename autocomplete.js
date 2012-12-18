4086162a5055ae8079d116bff08418db8f3028df
Boaz Leskes <b.leskes@gmail.com>
2012-12-18 23:50:51 +0100

    More autocomplete knowledge

--- autocomplete.js
+++ autocomplete.js
@@ -1,29 +1,53 @@
 
 
 var ES_SCHEME_BY_ENDPOINT = {
-    '_cluster/nodes/stats' : {
+  '_cluster/nodes/stats': {
 
-    },
-    '_percolator' : {
+  },
+  '_percolator': {
 
-    },
-    '_search' : {
-      autocomplete_rules: {
-        query : { term: {}, match_all : {}},
-        facets : { "*" : { terms : { field : [] } } },
-        size: {},
-        from: {},
-        search_type: {},
-        fields : {},
-        partial_fields : {},
-        highlight : {}
+  },
+  '_search': {
+    autocomplete_rules: {
+      query: {
+        term: {},
+        terms: { minimun_match : {}},
+        match_all: {},
+        match: {},
+        filtered: {
+          query: {},
+          filter: {}
+        },
+        ids: { type: {}, values: {} },
+        bool : {
+          must : {},
+          must_not : {},
+          should : {},
+          minimum_number_should_match: {},
+          boost: {}
+        },
+        field : {
+          "*" : {
+          query : {}, boost : {}, enable_position_increments: {}
+        } }
 
-      }
-    },
-    '_stats' : {
-    },
-    '_status' : {
+      },
+      facets: { "*": { terms: { field: [] } } },
+      size: {},
+      from: {},
+      search_type: {},
+      fields: {},
+      partial_fields: {},
+      highlight: {
+        pre_tags: {}, post_tags: {}, tags_schema: {},
+        fields: { "*": { "fragment_size": {}, "number_of_fragments": {} }}}
 
     }
+  },
+  '_stats': {
+  },
+  '_status': {
+
+  }
 };
 
