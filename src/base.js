b4fc8688156447a290a342151b5c69652228985f
Boaz Leskes <b.leskes@gmail.com>
2013-07-22 13:03:25 +0200

    version bump

--- src/base.js
+++ src/base.js
@@ -243,7 +243,7 @@ _gaq.push(['_setAccount', 'UA-11830182-16']);
 _gaq.push(['_setCustomVar',
    1,                // This custom var is set to slot #1.  Required parameter.
    'Version',    // The name of the custom variable.  Required parameter.
-   '0.7.5',        // The value of the custom variable.  Required parameter.
+   '0.7.7',        // The value of the custom variable.  Required parameter.
    1                 // Sets the scope to visitor-level.  Optional parameter.
 ]);
 
