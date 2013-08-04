b372bc901bca61d2d18d056f089102b1b5e3783b
Boaz Leskes <b.leskes@gmail.com>
2013-08-04 20:41:48 +0200

    Reduced size of method selector to make page fits of smaller screens

--- src/base.js
+++ src/base.js
@@ -239,7 +239,7 @@ $(document).ready(init);
 
 /* google analytics */
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-11830182-15']);
+_gaq.push(['_setAccount', 'UA-11830182-16']);
 _gaq.push(['_setCustomVar',
    1,                // This custom var is set to slot #1.  Required parameter.
    'Version',    // The name of the custom variable.  Required parameter.
